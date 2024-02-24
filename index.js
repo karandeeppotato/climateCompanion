5438f5e73cfc2d5baf5ef3fd45c645253b554be8
karandeeppotato <karan.sehgal.301202@gmail.com>
2024-02-24 22:29:21 +0530

    .

--- index.js
+++ index.js
@@ -10,26 +10,12 @@ const API_URL = "api.openweathermap.org/data/2.5/forecast";
 const API_KEY = "0773f06d983e61482c094fb56f2cc21a";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  try {
-    const ip =
-      req.ip ||
-      req.connection.remoteAddress ||
-      req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress;
-    const location = geolocateUser(ip);
-    console.log(location.ll[0] + " " + location.ll[1]);
-  } catch (error) {
-    console.log(error.message);
-  }
+    res.render('index.ejs');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-function geolocateUser(ip) {
-  const geo = geoip.lookup(ip);
-  return geo;
-}
