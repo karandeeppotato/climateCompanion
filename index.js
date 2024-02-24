5d63e9dc618a4d4cefb06c409add4bd43218c192
karandeeppotato <karan.sehgal.301202@gmail.com>
2024-02-24 23:01:39 +0530

    .

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ import geoip from "geoip-lite";
 
 const app = express();
 const port = 3000;
-const API_URL = "api.openweathermap.org/data/2.5/forecast";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast";
 const API_KEY = "0773f06d983e61482c094fb56f2cc21a";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.render('index.ejs');
+app.get('/', async (req, res) => {
+    try{
+        const response = await axios.get("https://api.openweathermap.org/data/2.5/forecast?q=New%20Delhi,%20IN&appid=0773f06d983e61482c094fb56f2cc21a&units=metric");
+        const result = response.data;
+        console.log(result);
+    } catch (error) {
+        console.log(error.message);
+    }
+    
 });
 
 app.listen(port, () => {
