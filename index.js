ada64fe519424162161e144dc29ca34001ab1336
Ash-159 <singh.ash1509@gmail.com>
2024-02-25 11:15:44 +0530

    .

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = 3000;
 const API_URL = "https://api.openweathermap.org/data/2.5/forecast";
 const API_KEY = "0773f06d983e61482c094fb56f2cc21a";
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,6 +18,7 @@ app.get('/', async (req, res) => {
         const response = await axios.get(API_URL+"?q=New%20Delhi,%20IN&appid="+API_KEY+"&units=metric");
         const result = response.data;
         console.log(result);
+        res.render('index.ejs');
     } catch (error) {
         console.log(error.message);
     }
