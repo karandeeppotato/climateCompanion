452c2b7e56d0abe9d55ed59a89c75f329ea9b1b0
karandeeppotato <karan.sehgal.301202@gmail.com>
2024-02-27 15:51:54 +0530

    .

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const API_URL_CURRENT =
   "https://api.openweathermap.org/data/2.5/weather?units=metric";
 const API_KEY = "0773f06d983e61482c094fb56f2cc21a";
 
+// app.use((req, res, next) => {
+//   if (req.url.endsWith('.js')) {
+//     res.setHeader('Content-Type', 'application/javascript');
+//   }
+//   next();
+// });
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -50,6 +56,45 @@ app.get("/weather", async (req, res) => {
     };
     res.render("index.ejs", content);
   } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.post("/weather", async (req, res) => {
+  try {
+    // console.log(req.body["searchBar"]);
+    const searchItem = req.body["searchBar"];
+    const response_fr = await axios.get(
+      API_URL_FORECAST + `&q=${searchItem}&appid=` + API_KEY
+    );
+    const forecast = response_fr.data;
+    const response_curr = await axios.get(
+      API_URL_CURRENT + `&q=${searchItem}&appid=` + API_KEY
+    );
+    const currData = response_curr.data;
+    const content = {
+      // CURRENT DATA
+      currentTemperature: currData.main.temp,
+      currentCity: currData.name,
+      currentCountry: currData.sys.country,
+      currentHumidity: currData.main.humidity,
+      currentFeelsLike: currData.main.feels_like,
+      currentMinTemp: currData.main.temp_min,
+      currentMaxTemp: currData.main.temp_max,
+      currentDescription: currData.weather[0].description,
+      currentDescriptionId: currData.weather[0].id,
+      currentPressure: currData.main.pressure,
+      currentVisibility: currData.visibility,
+      currentWindSpeed: currData.wind.speed,
+      currentSunrise: currData.sys.sunrise,
+      currentSunset: currData.sys.sunset,
+
+      // TIMELINE AND FORECAST
+      tl: forecast.list,
+    };
+    res.render("index.ejs", content);
+
+  } catch(error) {
     console.log(error.message);
   }
 });
